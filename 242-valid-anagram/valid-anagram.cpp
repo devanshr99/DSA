b9060715517e7d2b65b29f79fe493a2aa5d7d3c9
Devansh Rastogi <devanshrastogi993@gmail.com>
2026-06-24 16:30:52 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
+        //THIS IS BRUTFORCE METHOD
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s==t){
+        //     return true;
+        // }
+        // return false;
+
+        unordered_map<char, int> mp1;
+
+           for(char ch : s) {
+                mp1[ch]++;
+}
+ unordered_map<char, int> mp2;
+
+           for(char ch : t) {
+                mp2[ch]++;
+}
+        if(mp1==mp2){
             return true;
         }
-        return false;
+         return false;
     }
 };
