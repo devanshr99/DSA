f88f4b2046f3fccbeb6717dcd77bfe40448bc63c
Devansh Rastogi <devanshrastogi993@gmail.com>
2026-06-26 20:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (42.87%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        vector<int>ans;
-        if(n<1){
-            return false;
-        }
-        for(long i=2;i*i<=n;i++){
-               while(n%i==0){
-                   ans.push_back(i);
-                   n=n/i;
-               }
-        }
-        if(n>1){
-            ans.push_back(n);
-        }
-        for(int x:ans){
-            if(x != 2 && x != 3 && x != 5){
-                return false;
-            }
-        }
-        return true;
+    //     vector<int>ans;
+    //     if(n<1){
+    //         return false;
+    //     }
+    //     for(long i=2;i*i<=n;i++){
+    //            while(n%i==0){
+    //                ans.push_back(i);
+    //                n=n/i;
+    //            }
+    //     }
+    //     if(n>1){
+    //         ans.push_back(n);
+    //     }
+    //     for(int x:ans){
+    //         if(x != 2 && x != 3 && x != 5){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // 
+    if(n<1){
+        return false;
+    }
+    while(n%2==0)
+    n=n/2;
+    while(n%3==0)
+    n=n/3;
+    while(n%5==0)
+    n=n/5;
+    if(n == 1)
+    return true;
+else
+    return false;
     }
 };
