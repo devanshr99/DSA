61762104d209bdc7db575080771907b05327d60c
Devansh Rastogi <devanshrastogi993@gmail.com>
2026-07-09 15:33:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (50.77%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,39 +1,50 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        bool isallupper=true;
-        bool isalllower=true;
-        bool isfirstupper=true;
-           for(int i=0;i<word.size();i++){
-              if(!isupper(word[i])){
-                isallupper=false;
-                break;
-              }
-           }
-           for(int i=0;i<word.size();i++){
-              if(!islower(word[i])){
-                isalllower=false;
-                break;
-              }
-           }
-           if(!isupper(word[0]))
-            isfirstupper = false;
-           for(int i=1;i<word.size();i++){
-              if(!islower(word[i])){
-                isfirstupper=false;
-                break;
-              }
-           }
-          if(isallupper){
-            return true;
-          }
-          else if(isalllower){
-             return true;
-          }
-          else if(isfirstupper){
-            return true;
-          }
+        // bool isallupper=true;
+        // bool isalllower=true;
+        // bool isfirstupper=true;
+        //    for(int i=0;i<word.size();i++){
+        //       if(!isupper(word[i])){
+        //         isallupper=false;
+        //         break;
+        //       }
+        //    }
+        //    for(int i=0;i<word.size();i++){
+        //       if(!islower(word[i])){
+        //         isalllower=false;
+        //         break;
+        //       }
+        //    }
+        //    if(!isupper(word[0]))
+        //     isfirstupper = false;
+        //    for(int i=1;i<word.size();i++){
+        //       if(!islower(word[i])){
+        //         isfirstupper=false;
+        //         break;
+        //       }
+        //    }
+        //   if(isallupper){
+        //     return true;
+        //   }
+        //   else if(isalllower){
+        //      return true;
+        //   }
+        //   else if(isfirstupper){
+        //     return true;
+        //   }
         
-           return false;
+        //    return false;
+        //optimal hai but 3 baar traverse ho rha hai ..
+       int capital=0;
+       for(char ch:word){
+        if(isupper(ch)){
+            capital++;
+        }
+       }
+       if(capital==word.size()||capital==0||(capital==1 &&isupper(word[0]))){
+        return true;
+       }
+        return false;
     }
 };
