83019925906f3d9fe69ae99e9c6ca74ebd2f547c
Devansh Rastogi <devanshrastogi993@gmail.com>
2026-07-11 19:50:47 +0530

    Time: 27 ms (17.33%) | Memory: 24 MB (91.68%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -5,6 +5,23 @@ public:
         int ans;
         ans=abs((nums[0]*nums[1])-(nums[nums.size()-1]*nums[nums.size()-2]));
         return ans;
-        
+        // IT TAKE O(nlog n) DUE TO SORTING,SO think of approach that not uses sorting
+    //     int mini;
+    //     int maxi;
+    //     int a=INT_MIN;
+    //     int b=INT_MAX;
+    //     mini=*min_element(nums.begin(),nums.end());
+    //     maxi=*max_element(nums.begin(),nums.end());
+       
+    //    for(int i=0;i<nums.size();i++){
+    //     if(nums[i]<maxi){
+    //          a=max(a,nums[i]);
+    //     }
+    //     if(nums[i]>=mini){
+    //          b=min(b,nums[i]);
+    //     }
+
+    //    }
+    //     return abs((a*maxi)-(mini*b));
     }
 };
