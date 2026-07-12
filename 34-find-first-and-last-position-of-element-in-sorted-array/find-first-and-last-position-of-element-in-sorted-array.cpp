28a4c52649d1241bdc7a0c012108c6ebc3749e4c
Devansh Rastogi <devanshrastogi993@gmail.com>
2026-07-12 19:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (19.72%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+     vector<int> searchRange(vector<int>& nums, int target) {
 
-        unordered_map<int, pair<int, int>> mp;
+    //     unordered_map<int, pair<int, int>> mp;
 
-        for (int i = 0; i < nums.size(); i++) {
+    //     for (int i = 0; i < nums.size(); i++) {
 
-            if (mp.find(nums[i]) == mp.end()) {
-                mp[nums[i]] = {i, i};      // first occurrence
-            }
-            else {
-                mp[nums[i]].second = i;    // update last occurrence
-            }
-        }
+    //         if (mp.find(nums[i]) == mp.end()) {
+    //             mp[nums[i]] = {i, i};      // first occurrence
+    //         }
+    //         else {
+    //             mp[nums[i]].second = i;    // update last occurrence
+    //         }
+    //     }
 
-        if (mp.find(target) != mp.end()) {
-            return {mp[target].first, mp[target].second};
-        }
+    //     if (mp.find(target) != mp.end()) {
+    //         return {mp[target].first, mp[target].second};
+    //     }
+    //     return {-1, -1};
+     vector<int>ans;
+        vector<int> arr = {-1, -1};
 
-        return {-1, -1};
+for(int i=0;i<nums.size();i++){
+    if(nums[i]==target){
+        ans.push_back(i);
+        
+    }
+}
+if(ans.size()==0){
+    return arr;
+}
+else if(ans.size()==1){
+    return{ans[0],ans[0]};
+}
+else{
+    return {ans[0], ans[ans.size()-1]};
+}
     }
 };
