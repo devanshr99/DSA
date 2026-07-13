d6098037689122873f84094c43388945798d3966
Devansh Rastogi <devanshrastogi993@gmail.com>
2026-07-13 23:04:19 +0530

    Time: 38 ms (22.85%) | Memory: 65.2 MB (41.61%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -7,13 +7,20 @@ public:
         int j=1;
         int ans=0;
         while(j<nums.size()){
-            if(nums[i]==nums[j]){
-                return nums[i];
-            }
+            if(nums[i]==nums[j]){ //time complexity=o(nlog n)
+                return nums[i]; // space complexity-o(1)
+            }                  
             
                 i++;
                 j++;
      }
         return -1;;
-    }
+    //     unordered_map<int,int> mp;
+
+    // for (int x:nums) {
+    // mp[x]++;                   //time complexity=o(n)
+    // if (mp[x] > 1)             //space complexity=o(n)
+    //     return x;
+}
+    
 };
