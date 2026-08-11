4cd1e389240bbf707c8964f9c6bc8d586e8aa0cc
Devansh Rastogi <devanshrastogi993@gmail.com>
2026-08-11 21:52:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.18%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        int a=1;
+        int b=2;
+        for(int i=3;i<=45;i++){
+            int sum=a+b;
+            if(n==i){
+                
+                return sum;
+            }
+            a=b;
+            b=sum;
+
+        }
+        return n;
+         
+    }
+};
