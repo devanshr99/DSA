55e67a5358584e0e1f82212bfe4be4a353139d96
Devansh Rastogi <devanshrastogi993@gmail.com>
2026-08-18 22:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (12.56%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1){
+            return true;
+        }
+        while(n>=2){
+            if(n%2==0){
+              if(n==2){
+                return true;
+            }
+            n=n/2;
+        }
+        else{
+            return false;
+        }
+            }
+            
+       
+         return false;
+        
+    }
+};
